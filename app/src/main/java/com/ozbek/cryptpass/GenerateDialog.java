82a591accea4f68592b51e4567fb69d9289fe717
omaroz92 <omar.humber@outlook.com>
2019-05-01 01:21:08 -0400

    made changes to the dialog

--- app/src/main/java/com/ozbek/cryptpass/GenerateDialog.java
+++ app/src/main/java/com/ozbek/cryptpass/GenerateDialog.java
@@ -2,14 +2,15 @@ package com.ozbek.cryptpass;
 
 import androidx.annotation.NonNull;
 import android.app.Dialog;
-import android.content.Context;;
+import android.content.Context;
+import android.widget.Button;;
 
 class GenerateDialog extends Dialog {
-
-    //The dialog shows but it covers the whole screen and its black.  Fix it
 
     GenerateDialog(@NonNull Context context) {
         super(context, android.R.style.Theme_NoTitleBar_Fullscreen);
         setContentView(R.layout.dialog_layout);
+
+        Button saveButton = findViewById(R.id.btn_save);
     }
 }
